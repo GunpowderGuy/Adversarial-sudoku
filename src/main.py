91d5015ccd15ca1acbe48cf9a888cf36ba9df1e4
Diego Antonio Rosario Palomino <diegoosario@localhost.localdomain>
2018-10-19 12:12:03 -0500

    vertical checking implmented

--- src/main.py
+++ src/main.py
@@ -3,17 +3,18 @@ import operator
 
 N = 2
 
-matrix = [[y*x for y in range(N*N)] for x in range(N*N)]
+matrix = [[y*x for y in range(1,N*N+1)] for x in range(1,N*N+1)]
 
 protounique = lambda lista : len(lista)  == len(set(lista))
 
 sliceVertical = lambda  lista, fila : list(map(lambda x : x[fila] ,lista))
 
 def sudoku(board):
-     # horizontal = reduce( operator.and_, map( protounique , board))
+     horizontal = reduce( operator.and_, map( protounique , board))
      flippedBoard = list(map(lambda x : sliceVertical(board,x),range(N*N)))
-     return flippedBoard
+     vertical = reduce( operator.and_, map( protounique , flippedBoard))
+     return vertical and horizontal
 
 filtro = lambda lista : list(filter(None.__ne__,lista))
 
-print(matrix , sep ="\n")
+print(sudoku(matrix) , sep ="\n")
