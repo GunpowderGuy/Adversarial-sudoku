5bc13172c2eb17f1017e027fc66251023e651256
Diego Antonio Rosario Palomino <diegoosario@localhost.localdomain>
2018-10-19 10:43:57 -0500

    test2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello")
+N = 2
+
+nomatrix = [[0 for y in range(N*N)] for x in range(N*N)]
+
+protounique = lambda lista : len(lista)  == len(set(lista))
+
+filtro = lambda lista : list(filter(None.__ne__,lista))
+
+print(filtro([None,1]) , sep ="\n")
