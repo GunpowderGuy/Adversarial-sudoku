794d71126ce0ac1c88069428fdbd1c3e56df1d74
Diego Antonio Rosario Palomino <diegoosario@localhost.localdomain>
2018-10-20 01:30:59 -0500

    implementing quadrants

--- src/main.py
+++ src/main.py
@@ -5,9 +5,29 @@ N = 2
 
 matrix = [[y*x for y in range(1,N*N+1)] for x in range(1,N*N+1)]
 
+x = list(map(lambda x:x[:2], matrix))
+
 protounique = lambda lista : len(lista)  == len(set(lista))
 
-sliceVertical = lambda  lista, fila : list(map(lambda x : x[fila] ,lista))
+sliceVertical = lambda lista, columna: list(map(lambda x : x[fila] ,lista))
+
+def mul(x,y):
+    if x == 0 and y != 0 :
+        x = 1
+    if y == 0 and x != 0 :
+        y = 1
+    return x*y
+
+def cuandrantes(lista):
+    result = list(map(lambda x : [] , range(N*N)))
+    for cuadrante in range(N*N):
+        for y in range(N*N):
+            for x in range(N):
+                if x < mul(N,cuadrante) and y < mul(N,cuadrante):
+                #result[y].insert([y][x],lista)
+                    result[cuadrante].append(lista[y][x])
+    return result
+
 
 def sudoku(board):
      horizontal = reduce( operator.and_, map( protounique , board))
@@ -17,4 +37,11 @@ def sudoku(board):
 
 filtro = lambda lista : list(filter(None.__ne__,lista))
 
-print(sudoku(matrix) , sep ="\n")
+matriz = [
+[1,0,0,2],
+[0,0,0,0],
+[0,0,0,0],
+[3,0,0,4],
+]
+
+print(cuandrantes(matriz), sep ="\n")
